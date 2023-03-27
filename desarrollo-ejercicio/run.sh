cd17d8c5f5f3090d184ee58cfcfcc5aed97b2f23
Geraruiz89 <guruizg@desarrollo-ultrasist.com.mx>
2023-03-27 16:03:22 -0600

    gera10

--- desarrollo-ejercicio/run.sh
+++ desarrollo-ejercicio/run.sh
@@ -1,10 +1,10 @@
 docker stop segundo-examen
 docker rm segundo-examen
 
-docker pull ghcr.io/Geraruiz89/segundo-examen-access
+docker pull ghcr.io/geraruiz89/segundo-examen-access
 
 docker run -d \
 --name=segundo-examen \
 --restart=unless-stopped \
 -p7070:8080 \
-ghcr.io/Geraruiz89/segundo-examen-access
+ghcr.io/geraruiz89/segundo-examen-access
