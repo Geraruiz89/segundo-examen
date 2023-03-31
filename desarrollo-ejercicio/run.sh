ec7b82f076f3866f77444556c1e266f95ef72700
Geraruiz89 <guruizg@desarrollo-ultrasist.com.mx>
2023-03-31 17:19:36 -0600

    agregando mvn sonarcloud a actions

--- desarrollo-ejercicio/run.sh
+++ desarrollo-ejercicio/run.sh
@@ -1,11 +1,6 @@
-docker stop segundo-examen
-docker rm segundo-examen
-
-docker pull ghcr.io/geraruiz89/segundo-examen-access
-
-docker run -d \
---name=segundo-examen \
---restart=unless-stopped \
--p7070:8080 \
-ghcr.io/geraruiz89/segundo-examen-access
-
+curl  POST 'https://access.qbits.mx/api/login' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "user":"gus@aol.com",
+    "cred":"tavo"
+}'
